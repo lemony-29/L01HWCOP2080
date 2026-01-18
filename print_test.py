4a5051d533e6debbdc556899bffe601fcaa9e657
Ymani Williams <ywilliams0526@floridapoly.edu>
2026-01-18 06:41:18 +0000

    hw1

---
+++ print_test.py
@@ -0,0 +1,6 @@
+my_name = "Ymani Williams"
+
+print(my_name)
+print("Use 4 spaces per indentation level.")
+print("Python comments start with the hash character (#) that is not part of a string literal, and ends at the end of a physical line.")
+print("By the end of the course, I hope to:\n1. Advance my skills in Python much greater than where they are now\n2. Learn more information about programming (in general)\n3. Become motivated to use Python more (for projects and such)")
